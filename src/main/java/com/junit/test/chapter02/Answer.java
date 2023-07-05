f138be4780bc9b2021fceef5e026929c9113fae2
OiKimiO <etkim02@naver.com>
2023-07-05 21:25:44 +0900

    code. chapter2 - 문제와 정답을 만들어라
    - abstract class: Question
    - class: Answer
    - class: BooleanQuestion
    - class: PercentileQuestion

---
+++ src/main/java/com/junit/test/chapter02/Answer.java
@@ -0,0 +1,32 @@
+package com.junit.test.chapter02;
+
+public class Answer {
+    private int i;
+    private Question question;
+
+    public Answer(int i, Question question) {
+        this.i = i;
+        this.question = question;
+    }
+
+    public String getQuestionText(){
+        return question.getText();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", question.getText(), question.getAnswerChoices(i));
+    }
+
+    public boolean match(int expected){
+        return question.match(expected,i);
+    }
+
+    public boolean match(Answer otherAnswer){
+        return question.match(i, otherAnswer.i);
+    }
+
+    public Question getCharacteristic(){
+        return question;
+    }
+}
--- src/main/java/com/junit/test/chapter02/Question.java
+++ src/main/java/com/junit/test/chapter02/Question.java
@@ -1,2 +1,36 @@
-package com.junit.test.chapter02;public class Question {
+package com.junit.test.chapter02;
+
+public abstract class Question {
+    private String text;
+    private String[] answerChoices;
+    private int id;
+
+    public Question(int id, String text, String[] answerChoices) {
+        this.id = id;
+        this.text = text;
+        this.answerChoices = answerChoices;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAnswerChoices(int i) {
+        return answerChoices[i];
+    }
+
+    public boolean match(Answer answer){
+        return false;
+    }
+
+    abstract public boolean match(int expected, int actual);
+
+    public int indexOf(String matchingAnswerChoice){
+        for (int i = 0; i < answerChoices.length; i++) {
+            if(answerChoices[i].equals(matchingAnswerChoice)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
