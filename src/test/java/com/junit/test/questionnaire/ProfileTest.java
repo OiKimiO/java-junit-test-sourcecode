cb4a0e19cfe2b7c10bd993aaf9734a71f4a32a01
OiKimiO <etkim02@naver.com>
2023-07-10 19:58:34 +0900

    test. chapter2 - dontcare_기준을_충족못해도_답변은_true를 리턴하라
    - profile answer = qeustion -> false
    - criteria answer = question -> true
    - criterion -> criteria answer, DontCare

--- src/test/java/com/junit/test/questionnaire/ProfileTest.java
+++ src/test/java/com/junit/test/questionnaire/ProfileTest.java
@@ -3,6 +3,7 @@ package com.junit.test.questionnaire;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProfileTest {
 
@@ -24,4 +25,21 @@ public class ProfileTest {
         assertFalse(matches);
     }
 
+    @Test
+    void dontcare_기준을_충족못해도_답변은_true(){
+        Profile profile = new Profile("Bull Hockey, inc");
+        Question question = new BooleanQuestion(1, "Got milk?");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer,
+                                            Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
+    }
 }
