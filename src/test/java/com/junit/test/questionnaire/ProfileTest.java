bd0638c56a89de3972f25ba0c5fbe11d731f9123
OiKimiO <etkim02@naver.com>
2023-07-11 19:32:57 +0900

    test. chapter2 - 리팩터링: 공통으로 사용되는 Profile, Question, Criteria를 @BeforeEach로 표현하라
    - 상호 코드에  영향을 줄 수 있는 static 작성을 지양합니다.

--- src/test/java/com/junit/test/questionnaire/ProfileTest.java
+++ src/test/java/com/junit/test/questionnaire/ProfileTest.java
@@ -1,5 +1,6 @@
 package com.junit.test.questionnaire;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,14 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProfileTest {
 
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @BeforeEach
+    public void create(){
+        profile = new Profile("Bull Hockey, inc");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
     @Test
     void mustMatch_기준을_충족못하면_답변은_false(){
-        Profile profile = new Profile("Bull Hockey, inc");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer,
                                             Weight.MustMatch);
@@ -27,12 +36,9 @@ public class ProfileTest {
 
     @Test
     void dontcare_기준을_충족못해도_답변은_true(){
-        Profile profile = new Profile("Bull Hockey, inc");
-        Question question = new BooleanQuestion(1, "Got milk?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer,
                                             Weight.DontCare);
